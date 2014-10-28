466bf47f43fb768adda969cbbe5b2e1171e0caa9
joelcoxokc <joelcox@hisimagination.com>
2014-10-28 14:17:11 -0700

    Update: server-test sub-generator Refactor code, wrap in IFFE

--- generators/server/test/templates/base/_.model.js
+++ generators/server/test/templates/base/_.model.js
@@ -1,17 +1,22 @@
-'use strict';
+;(function(){
+    'use strict';
 
-/**
- * Module dependencies.
- */
-var mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
+    /**
+     * Module dependencies.
+     */
+    var mongoose = require('mongoose'),
+      Schema = mongoose.Schema;
 
-/**
- * <%= names.single.classed %> Schema
- */
-var <%= names.single.classed %>Schema = new Schema({
-  // <%= names.single.classed %> model fields
-  // ...
-});
+    /**
+     * [Schema <%= names.single.classed %> Schema]
+     * @type {Schema}
+     */
+    var <%= names.single.classed %>Schema = new Schema({
+      // <%= names.single.classed %> model fields
+      // ...
+    });
 
-module.exports = mongoose.model('<%= names.single.classed %>', <%= names.single.classed %>Schema);
+    module.exports = mongoose.model('<%= names.single.classed %>', <%= names.single.classed %>Schema);
+
+
+}).call(this);
